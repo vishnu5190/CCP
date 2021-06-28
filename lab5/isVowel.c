d37965426ab87ea53baff03fe0c21a954ad3a24e
vishnu kr <79283613+vishnu5190@users.noreply.github.com>
2021-06-28 10:40:03 +0530

    Add files via upload

---
+++ lab5/isVowel.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+// printf(), scanf()
+#include <stdbool.h>
+// bool
+
+bool is_vowel(char x);
+// checks if x is a vowel
+
+int main() {
+
+    printf("Enter a character: ");
+    char input;
+    scanf("%c", &input);
+
+    printf(
+	is_vowel(input) ?
+		"The entered character is a vowel\n"
+		:
+		"The entered character is consonant\n"
+    );
+
+    return 0;
+}
+
+
+bool is_vowel(char x) {
+
+    switch(x) {
+
+	case 'a': case 'e': 
+	case 'i': case 'o':
+	case 'u':
+	case 'A': case 'E': 
+	case 'I': case 'O':
+	case 'U':
+
+	    return true;
+
+	default:
+
+	    return false;
+    }
+}
